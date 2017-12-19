8de5e817a995b3d2e7acdfa0013e70b0c3b47942
MarcoKaldenbach <34687207+MarcoKaldenbach@users.noreply.github.com>
2017-12-19 17:18:45 +0100

    Uploading the FastSerial files

---
+++ FastSerial.cpp
@@ -0,0 +1,181 @@
+/*
+  FastSerial.cpp - Fast serial library for Arduino
+  Copyright (c) 2017 Marco Kaldenbach (marcokaldenbach@gmail.com).  All right reserved.
+
+  Implemented the basics derived from the Arduino HardwareSerial driver:
+  Copyright (c) 2006 Nicholas Zambetti.  All right reserved.
+  
+  See also the LICENSE file for licenses
+*/
+
+
+#include "FastSerial.h"
+
+
+/* Define the Receive Interrupt function when it has not been defined already */
+#if !defined(USART0_RX_vect) && defined(USART1_RX_vect)
+// do nothing - on the 32u4 the first USART is USART1
+#else
+#if !defined(USART_RX_vect) && !defined(USART0_RX_vect) && \
+    !defined(USART_RXC_vect)
+  #error "Don't know what the Data Received vector is called for the first UART"
+#elif !defined(serialEvent_implemented)
+  //only define interrupt when it has not already been defined
+  #define serialEvent_implemented
+#if defined(USART_RX_vect)
+  ISR(USART_RX_vect)
+#elif defined(USART0_RX_vect)
+  ISR(USART0_RX_vect) 
+#elif defined(USART_RXC_vect)
+  ISR(USART_RXC_vect) // ATmega8
+#endif
+  {
+    FastSerial0.received_interrupt();
+  }
+#endif
+#endif
+
+#if defined(USART1_RX_vect) && !defined(serialEvent1_implemented)
+  #define serialEvent1_implemented
+ISR(USART1_RX_vect)
+{
+  FastSerial1.received_interrupt();
+}
+#endif
+
+#if defined(USART2_RX_vect) && defined(UDR2) && !defined(serialEvent2_implemented)
+  #define serialEvent2_implemented
+ISR(USART2_RX_vect)
+{
+  FastSerial2.received_interrupt();
+}
+#endif
+
+#if defined(USART3_RX_vect) && defined(UDR3) && !defined(serialEvent3_implemented)
+  #define serialEvent3_implemented
+ISR(USART3_RX_vect)
+{
+  FastSerial3.received_interrupt();
+}
+#endif
+
+/* store_char(unsigned char, ring_buffer *) function copied from HardwareSerial.cpp */
+inline void store_char(unsigned char c, ring_buffer *buffer)
+{
+  int i = (unsigned int)(buffer->head + 1) % SERIAL_BUFFER_SIZE;
+
+  // if we should be storing the received character into the location
+  // just before the tail (meaning that the head would advance to the
+  // current location of the tail), we're about to overflow the buffer
+  // and so we don't write the character or advance the head.
+  if (i != buffer->tail) {
+    buffer->buffer[buffer->head] = c;
+    buffer->head = i;
+  }
+}
+
+FastSerial::FastSerial(volatile uint8_t * ubrrh, volatile uint8_t *ubrrl,
+                       volatile uint8_t *ucsra, volatile uint8_t *ucsrb,
+                       volatile uint8_t *ucsrc, volatile uint8_t *udr):
+                       _ubrrh(ubrrh),
+                       _ubrrl(ubrrl),
+                       _ucsra((volatile UCSRA_bits*)ucsra),
+                       _ucsrb((volatile UCSRB_bits*)ucsrb),
+                       _ucsrc((volatile UCSRC_bits*)ucsrc),
+                       _udr(udr)
+{
+  //Initialise the rx head and tail values
+  _rx_buffer.head = 0;
+  _rx_buffer.tail = 0;
+}
+
+void FastSerial::begin(long baud)
+{
+    cli();
+    *_ubrrh = ((F_CPU / 16 + baud / 2) / baud - 1) >> 8;
+    *_ubrrl = ((F_CPU / 16 + baud / 2) / baud - 1); 				// Set the baud rate prescale rate register
+    sei();
+    
+    interrupt_init();
+}
+
+void FastSerial::interrupt_init(void)
+{
+    cli();
+    // Macro to determine the baud prescale rate see table 22.1 in the Mega datasheet
+
+    // Enable receiver and transmitter and Rx interrupt
+    _ucsrb->bits.RXEN = 1;
+    _ucsrb->bits.TXEN = 1;
+    _ucsrb->bits.RXCIE = 1;
+
+    // Set frame format: 8data, 1 stop bit. See Table 22-7 for details
+    _ucsrc->bits.USBS = 1;
+    _ucsrc->bits.UCSZ0= 1;
+    _ucsrc->bits.UCSZ1= 1;
+    
+    sei();
+}
+
+size_t FastSerial::write(uint8_t data)
+{
+    //loop while the transmit buffer is not empty
+    while(_ucsra && _ucsra &&_ucsra->bits.UDRE != 1);
+
+    //when the buffer is empty write data to the transmitted
+    *_udr = data;
+    
+    return 1;
+}
+
+size_t FastSerial::write(uint8_t* data, uint8_t data_size)
+{
+    size_t N = 0;
+    for(int i = 0; i < data_size; i++)
+        N += write(data[i]);
+    return N;
+}
+
+int FastSerial::read()
+{
+  // if the head isn't ahead of the tail, we don't have any characters
+  if (_rx_buffer.head == _rx_buffer.tail) {
+    return -1;
+  } else {
+    unsigned char c = _rx_buffer.buffer[_rx_buffer.tail];
+    _rx_buffer.tail = (unsigned int)(_rx_buffer.tail + 1) % SERIAL_BUFFER_SIZE;
+    return c;
+  }
+}
+
+int FastSerial::available()
+{
+  return (int)(SERIAL_BUFFER_SIZE + _rx_buffer.head - _rx_buffer.tail) % SERIAL_BUFFER_SIZE;
+}
+
+void FastSerial::received_interrupt() /* Handle the receive interrupt */
+{
+    if (bit_is_clear(*_ucsra, _ucsra->bits.UPE)) {  
+      char c = *_udr;
+      store_char(c, &_rx_buffer);
+    }
+}
+
+/*--------------------- Preinstantiate Objects ---------------------*/
+#if defined(UBRRH) && defined(UBRRL)
+  FastSerial FastSerial0(&UBRRH, &UBRRL, &UCSRA, &UCSRB, &UCSRC, &UDR);
+#elif defined(UBRR0H) && defined(UBRR0L)
+  FastSerial FastSerial0(&UBRR0H, &UBRR0L, &UCSR0A, &UCSR0B, &UCSR0C, &UDR0);
+#else
+  #error no serial port defined  (port 0)
+#endif
+
+#if defined(UBRR1H)
+  FastSerial FastSerial1(&UBRR1H, &UBRR1L, &UCSR1A, &UCSR1B, &UCSR1C, &UDR1);
+#endif
+#if defined(UBRR2H)
+  FastSerial FastSerial2(&UBRR2H, &UBRR2L, &UCSR2A, &UCSR2B, &UCSR2C, &UDR2);
+#endif
+#if defined(UBRR3H)
+  FastSerial FastSerial3(&UBRR3H, &UBRR3L, &UCSR3A, &UCSR3B, &UCSR3C, &UDR3);
+#endif
---
+++ FastSerial.h
@@ -0,0 +1,144 @@
+/*
+  FastSerial.h - Fast serial library headerfile for Arduino
+  Copyright (c) 2017 Marco Kaldenbach (marcokaldenbach@gmail.com).  All right reserved.
+
+  Implemented the basics derived from the Arduino HardwareSerial driver:
+  Copyright (c) 2006 Nicholas Zambetti.  All right reserved.
+  
+  See also the LICENSE file for licenses
+*/
+
+#ifndef FastSerial_h
+#define FastSerial_h
+
+#include <inttypes.h>
+
+#include <avr/interrupt.h>
+
+#include <stddef.h>
+
+/* SERIAL_BUFFER_SIZE and ring_buffer, same definitions as HardwareSerial.cpp */
+#if (RAMEND < 1000)
+  #define SERIAL_BUFFER_SIZE 16
+#else
+  #define SERIAL_BUFFER_SIZE 64
+#endif
+
+struct ring_buffer
+{
+  unsigned char buffer[SERIAL_BUFFER_SIZE];
+  volatile unsigned int head;
+  volatile unsigned int tail;
+};
+
+/* unions for the UCSRA, UCSRB, UCSRC definitions as decribed by Mika Tuupola (https://www.appelsiini.net/2011/simple-usart-with-avr-libc) */
+typedef union
+{
+    struct
+    {
+        // UCSRA bit values:
+        unsigned MPCM   :  1;   // bit 0	MPCM	Multi-processor Communication Mode. When set incoming data is ignored if no addressing information is provided.
+        unsigned U2X    :  1;   // bit 1	U2X     USART Double Transmission Speed. When set decreases the bit time by half doubling the speed.
+        unsigned UPE    :  1;   // bit 2	UPE     USART Parity Error. Set when next frame in the UDR0 has a parity error.
+        unsigned DOR    :  1;   // bit 3	DOR     Data OverRun. Set when the UDR0 was not read before the next frame arrived.
+        unsigned FE     :  1;   // bit 4	FE      Frame Error. Set when next byte in the UDR0 register has a framing error.
+        unsigned UDRE   :  1;   // bit 5	UDRE	USART Data Register Empty. Set when the UDR0 register is empty and new data can be transmitted.
+        unsigned TXC    :  1;   // bit 6	TXC     USART Transmit Complete. Set when all data has transmitted.
+        unsigned RXC    :  1;   // bit 7	RXC     USART Receive Complete. Set when data is available and the data register has not be read yet.
+    } bits;
+    uint8_t UCSRA;
+} UCSRA_bits;
+
+
+typedef union
+{
+    struct
+    {
+        // UCSRB bit values:
+        unsigned TXB8   :  1;   // bit 0	TXB8	Transmit Data Bit 8. When using 8 bit transmission the 8th bit to be submitted.
+        unsigned RXB8   :  1;   // bit 1	RXB8	Receive Data Bit 8. When using 8 bit transmission the 8th bit received.
+        unsigned UCSZ2  :  1;   // bit 2	UCSZ2	USART Character Size 0. Used together with UCSZ01 and UCSZ00 to set data frame size. Available sizes are 5-bit (000), 6-bit (001), 7-bit (010), 8-bit (011) and 9-bit (111).
+        unsigned TXEN   :  1;   // bit 3	TXEN	Transmitter enable. Set to enable transmitter.
+        unsigned RXEN   :  1;   // bit 4	RXEN	Receiver Enable. Set to enable receiver.
+        unsigned UDRIE  :  1;   // bit 5	UDRIE	USART Data Register Empty Interrupt Enable. Set to allow data register empty interrupts.
+        unsigned TXCIE  :  1;   // bit 6	TXCIE	TX Complete Interrupt Enable. Set to allow transmission complete interrupts.
+        unsigned RXCIE  :  1;   // bit 7	RXCIE	RX Complete Interrupt Enable. Set to allow receive complete interrupts.
+    } bits;
+    uint8_t UCSRB;
+} UCSRB_bits;
+
+
+typedef union
+{
+    struct
+    {
+        // UCSRC bit values:
+        unsigned UCPOL  :  1;   // bit 0    UCPOL	USART Clock Polarity. Set to transmit on falling edge and sample on rising edge. Unset to transmit on rising edge and sample on falling edge.
+        unsigned UCSZ0  :  1;   // bit 1    UCSZ0   see line below
+        unsigned UCSZ1  :  1;   // bit 2    UCSZ1   see line below
+                                                // USART Character Size 1 and 0. Used together with with UCSZ2 to set data frame size. Available sizes are 5-bit (000), 6-bit (001), 7-bit (010), 8-bit (011) and 9-bit (111).
+        unsigned USBS   :  1;   // bit 3	USBS	USART Stop Bit Select. Set to select 1 stop bit. Unset to select 2 stop bits.
+        unsigned UPM0   :  1;   // bit 4    UPM0    see line below
+        unsigned UPM1   :  1;   // bit 5    UPM1    see line below
+                                                // USART Parity Mode 1 and 0. UPM1 and UPM0 select the parity. Available modes are none (00), even (10) and odd (11).
+        unsigned UMSEL0 :  1;   // bit 6    UMSEL0  see line below
+        unsigned UMSEL1 :  1;   // bit 7    UMSEL1  see line below
+                                                // USART Mode Select 1 and 0. UMSEL1 and UMSEL0 combined select the operating mode. Available modes are asynchronous (00), synchronous (01) and master SPI (11).
+    } bits;
+    uint8_t UCSRC;
+} UCSRC_bits;
+
+
+
+class FastSerial {
+public:
+    FastSerial( volatile uint8_t * ubrrh, volatile uint8_t *ubrrl,
+                volatile uint8_t *ucsra, volatile uint8_t *ucsrb,
+                volatile uint8_t *ucsrc, volatile uint8_t *udr);
+
+    void begin(long baud);
+    void interrupt_init(void);
+    size_t write(uint8_t data);
+    size_t write(uint8_t* data, uint8_t data_size);
+    inline size_t write(unsigned long n) { return write((uint8_t)n); }
+    inline size_t write(long n) { return write((uint8_t)n); }
+    inline size_t write(unsigned int n) { return write((uint8_t)n); }
+    inline size_t write(int n) { return write((uint8_t)n); }
+    int read();
+    int available();
+    void received_interrupt();
+
+private:
+
+
+protected:
+  // http://www.atmel.com/Images/Atmel-2549-8-bit-AVR-Microcontroller-ATmega640-1280-1281-2560-2561_datasheet.pdf
+  volatile uint8_t * const _ubrrh; // USART Baud Rate Register High Byte (only 4 bits)
+  volatile uint8_t * const _ubrrl; // USART Baud Rate Register Low Byte
+
+  // https://www.appelsiini.net/2011/simple-usart-with-avr-libc
+  volatile UCSRA_bits * const _ucsra;    // UCSRA  8 setting bits: | RXC    |  TXC    |  UDRE  |  FE   |  DOR  |  UPE   |  U2X   |  MPCM  |
+  volatile UCSRB_bits * const _ucsrb;    // UCSRB  8 setting bits: | RXCIE  |  TXCIE  |  UDRIE |  RXEN |  TXEN |  UCSZ2 |  RXB8  |  TXB8  |
+  volatile UCSRC_bits * const _ucsrc;    // UCSRC  8 setting bits: | UMSEL1 |  UMSEL0 |  UPM1  |  UPM0 |  USBS |  UCSZ1 |  UCSZ0 |  UCPOL |
+
+  volatile uint8_t * const _udr;    // USART Data Register
+ 
+  ring_buffer _rx_buffer;
+};
+
+/* Forward the FastSerial objects */
+#if defined(UBRRH) || defined(UBRR0H)
+  extern FastSerial FastSerial0;
+#endif
+#if defined(UBRR1H)
+  extern FastSerial FastSerial1;
+#endif
+#if defined(UBRR2H)
+  extern FastSerial FastSerial2;
+#endif
+#if defined(UBRR3H)
+  extern FastSerial FastSerial3;
+#endif
+
+
+#endif /* FastSerial_h */
